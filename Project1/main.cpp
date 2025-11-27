c783ebae92e2ab9ccdf79ecdb9157ad3339e68f7
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-11-28 00:46:29 +0800

    MCO2 Start
    Refactored Instruction, Process, CPUCore into classes instead of structs

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 
 int main() {
-    srand(static_cast<unsigned>(time(nullptr)));
+    srand(static_cast<unsigned int>(time(0)));
     std::cout << "Welcome to CSOPESY Emulator CLI\n";
     std::cout << "Developers: Go, Michael Joseph | Go, Michael Anthony | Magaling, Zoe | Uy, Matthew\n";
     std::cout << "Version date: 11/5/25\n\n";
